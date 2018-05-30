a6ab98d531efc334bda8bcbcf5a434e53d5c3f94
Admin <hoangtri.45@gmail.com>
2018-05-30 22:30:08 +0700

    WPF Button

---
+++ WPF_Button/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Button
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Button btn = new Button();
+            //btn.Content = "www.google.com";
+            //grd.Children.Add(btn);
+
+            //TextBlock txbl = new TextBlock();
+            //txbl.Text = "Free Education";
+            //btn.Content = txbl; // -> textblock sẽ nằm trong button, content chỉ chấp nhận 1 element
+
+            //TextBox txb = new TextBox();
+            //txb.Width = 100;
+            //txb.Height = 50;
+            //btn.Content = txb;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("おはようございます。");
+        }
+    }
+}
