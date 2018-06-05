fbdc617ae31deaf9d30697427b405043d0b8e253
Admin <hoangtri.45@gmail.com>
2018-06-05 16:08:03 +0700

    WPF Binding & Grid

---
+++ WPF_Binding/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {        
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            BtnName = "Binding data from code behide";
+        }
+
+        public string BtnName
+        {
+            get
+            {
+                return btnName;
+            }
+
+            set
+            {
+                btnName = value;
+                OnPropertyChanged("BtnName");
+            }
+        }
+        private string btnName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string newName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(newName));
+        }
+    }
+}
